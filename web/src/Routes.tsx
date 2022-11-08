09890088294001c110b82e08cb865d7e8d95b161
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2022-11-08 14:50:47 -0800

    Fixed route error, thanks so much @pantheredeye

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -16,8 +16,6 @@ const Routes = () => {
   return (
     <Router>
 
-      <Route path="/contact" page={ContactPage} name="contact" />
-
       <Set wrap={ScaffoldLayout} title="Posts" titleTo="posts" buttonLabel="New Post" buttonTo="newPost">
         <Route path="/posts/new" page={PostNewPostPage} name="newPost" />
         <Route path="/posts/{id:Int}/edit" page={PostEditPostPage} name="editPost" />
