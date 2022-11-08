fc27801beb9a3ac71dc289c6ba7f631daeae486c
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2022-11-08 13:00:10 -0800

    Through end of tutorial Chapter 2

--- web/src/layouts/BlogLayout/BlogLayout.tsx
+++ web/src/layouts/BlogLayout/BlogLayout.tsx
@@ -9,7 +9,7 @@ const BlogLayout = ({ children }: BlogLayoutProps) => {
       <>
       <header>
         <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
+          <Link to={routes.home()}>World Lab Blog</Link>
         </h1>
         <nav>
           <ul>
