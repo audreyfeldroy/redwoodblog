f85032763feda77044270712f4ff34830e548b7a
Audrey Roy Greenfeld <aroy@alum.mit.edu>
2022-11-09 14:33:56 -0800

    Through end of tutorial Chapter 3

--- web/src/pages/ContactPage/ContactPage.tsx
+++ web/src/pages/ContactPage/ContactPage.tsx
@@ -1,8 +1,10 @@
 import { MetaTags } from '@redwoodjs/web'
-import { Form, TextField, Submit, SubmitHandler } from '@redwoodjs/forms'
+import { FieldError, Form, Label, TextField, TextAreaField, Submit, SubmitHandler } from '@redwoodjs/forms'
 
 interface FormValues {
-  input: string
+  name: string,
+  email: string,
+  message: string
 }
 
 const ContactPage = () => {
@@ -14,8 +16,43 @@ const ContactPage = () => {
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit}>
-        <TextField name="input" />
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
+
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
+
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
+
         <Submit>Save</Submit>
       </Form>
     </>
